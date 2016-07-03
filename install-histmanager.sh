29327603bb5954cd1cfa39a5701fa4a2b85f5fb2
Kaivalya Rawal <histmanager@gmail.com>
2016-07-03 22:50:59 +0530

    Made Importable

--- install-histmanager.sh
+++ install-histmanager.sh
@@ -40,12 +40,34 @@ echo "Installing HistManagerBETA - report any errors to histmanager@gmail.com"
 sleep 1
 echo ""
 
-# Backup Existing History, if any
-if [ -f "~/.bash_history" ] # create .bash_history file if it doesn't exist
+#Backup .bashrc, if it exists
+if [ -f "~/.bashrc" ]
 then
-	sleep 0.1
+    cp ~/.bashrc ~/bashrc_backup.txt
+    {
+        sleep 0.25
+        printf "%s" "Creating backup of .bashrc file"
+        # as on $(date +%y-%m-%d)" #Shortened display
+        sleep 0.2
+        printf "%s" " ."
+        sleep 0.2
+        printf "%s" "."
+        sleep 0.2
+        printf "%s" "."
+        sleep 0.25
+        printf "%s" ". "
+        sleep 0.25
+        echo "[Done]"
+        sleep 1
+    } # Fancy Display!
+fi
+
+# Backup Existing History, if any
+if [ -f "~/.bash_history" ] # create .bash_history file if it doesn't exist (needed for import into Master.khtxt)
+then
+    sleep 0.1
 else
-	touch ~/.bash_history
+    touch ~/.bash_history
 fi
 cp ~/.bash_history ~/BashHistoryBackup_20$(date +%y-%m-%d_%H':'%M':'%S).txt
 {
